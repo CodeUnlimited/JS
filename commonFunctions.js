ce23c71b326f26cdd186416a186e4becd4977589
aacom007 <akshayan@usc.edu>
2017-02-10 23:15:08 -0800

    Common JS Library

---
+++ commonFunctions.js
@@ -0,0 +1,9 @@
+/* common functions*/
+
+function validateEmail(emailAddress) {
+
+    var emailRegex = /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    var match = emailAddress.toLowerCase().match(emailRegex);
+
+    return (match !== null && match.length === 1);
+}
